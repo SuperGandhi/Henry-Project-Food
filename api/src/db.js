5523846ec6dcb82e063e19cd13371a7799c1e45c
SuperGandhi <patrickborjabueno@hotmail.com>
2022-06-20 12:09:23 -0500

    add diet

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe, Diet } = sequelize.models;
+
+//Relacions
+Recipe.belongsToMany(Diet, {through: 'recipediet'});
+Diet.belongsToMany(Recipe, {through: 'recipediet'});
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
