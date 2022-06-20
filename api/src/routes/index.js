60e295c6fb5f8292c3a7289d59a4b121332c57c9
SuperGandhi <patrickborjabueno@hotmail.com>
2022-06-20 12:10:28 -0500

    add and list routes use

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,17 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const recipeRouter = require('./recipe');
+const recipesRouter = require('./recipes');
+const typesRouter = require('./types')
 
 const router = Router();
 
 // Configurar los routers
+router.use('/recipe', recipeRouter);
+router.use('/recipes', recipesRouter);
+router.use('/types', typesRouter);
+
+
 // Ejemplo: router.use('/auth', authRouter);
-
-
 module.exports = router;
